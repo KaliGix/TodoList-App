1516852e0018ca9c9f2699981899ccd5bf8072e8
Kalibell Dalmasi <kalibelldalmasi@gmail.com>
2026-03-14 23:13:07 -0400

    space removed

--- main.js
+++ main.js
@@ -6,8 +6,7 @@ const btnCompleted = document.querySelector("#btn-completed");
 const errorMessage = document.querySelector(".errorMessage");
 const taskList = document.querySelector(".task-list");
 
-taskList.addEventListener("change", function(event) {
-
+taskList.addEventListener("change", function (event) {
   if (event.target.classList.contains("checkbox-item")) {
     completedTask(event);
   }
@@ -26,15 +25,14 @@ function addTask() {
         name: addTaskInput.value.trim(),
         completed: false,
       };
-     
-  
-      if(saveTaskLocalStorage(newTask)){
-          //add the new object to the existing array
+
+      if (saveTaskLocalStorage(newTask)) {
+        //add the new object to the existing array
         task.push(newTask);
         taskId = newTask.id;
         errorMessageVisibility("none");
       }
-      
+
       return 1;
     }
     errorMessageVisibility("block");
@@ -45,30 +43,27 @@ function addTask() {
   return 0;
 }
 
-function errorMessageVisibility(visibility){
+function errorMessageVisibility(visibility) {
   errorMessage.style.display = visibility;
 }
 
-function saveTaskLocalStorage(newTask){
+function saveTaskLocalStorage(newTask) {
+  //get the localstorage data before adding the new object
+  let savedTask = JSON.parse(localStorage.getItem("task")) || [];
+  savedTask.push(newTask); // the new object is added to the array saveTask
 
-      //get the localstorage data before adding the new object
-      let savedTask = JSON.parse(localStorage.getItem("task")) || [];
-      savedTask.push(newTask); // the new object is added to the array saveTask
+  //localstorage save the date it had before and the new one, so
+  //that create a list
+  localStorage.setItem("task", JSON.stringify(savedTask));
 
-      //localstorage save the date it had before and the new one, so
-      //that create a list
-      localStorage.setItem("task", JSON.stringify(savedTask));
-
-      if (savedTask !== undefined) {
-        console.log("save successfully");
-        clearInputTaks();
-        renderTask(newTask.id);
-        return true;
-       
-      } 
-       console.log("it was not saved");
-       return false;
-     
+  if (savedTask !== undefined) {
+    console.log("save successfully");
+    clearInputTaks();
+    renderTask(newTask.id);
+    return true;
+  }
+  console.log("it was not saved");
+  return false;
 }
 function clearInputTaks() {
   addTaskInput.value = "";
@@ -76,40 +71,35 @@ function clearInputTaks() {
 
 function deleteTask() {}
 
-function completedTask(taskSelected){
-        
-      const getAllTask = JSON.parse(localStorage.getItem("task"));
-      let li = taskSelected.target.closest("li");
-      let id = li.dataset.id; 
-      
-      const updateTask = getAllTask.find(task => task.id == id);
+function completedTask(taskSelected) {
+  const getAllTask = JSON.parse(localStorage.getItem("task"));
+  let li = taskSelected.target.closest("li");
+  let id = li.dataset.id;
 
-      if(updateTask)
-         updateTask.completed = true;
- 
-      localStorage.setItem("task", JSON.stringify(getAllTask));
-      renderTask();
-      console.log(localStorage.getItem("task"));
+  const updateTask = getAllTask.find((task) => task.id == id);
+
+  if (updateTask) 
+      updateTask.completed = true;
+
+  localStorage.setItem("task", JSON.stringify(getAllTask));
+  renderTask();
 }
 
-function hideTaskUI(){
-  taskSelected.target.closest('li').style.display = "none";
+function hideTaskUI() {
+  taskSelected.target.closest("li").style.display = "none";
 }
 
 function renderTask() {
   const tasks = JSON.parse(localStorage.getItem("task"));
-   
-  taskList.innerHTML ="";
-  
-  if(tasks!= null){
-  tasks.forEach((task) => {
-    taskList.innerHTML += `<li class="task-item" data-id= ${task.id}><label for="task-item">
+
+  taskList.innerHTML = "";
+
+  if (tasks != null) {
+    tasks.forEach((task) => {
+      taskList.innerHTML += `<li class="task-item" data-id= ${task.id}><label for="task-item">
      <input type="checkbox" class="checkbox-item">
         ${task.name}    
         <button type="button" class="btn delete">x</button></label></li>`;
-
-  });
-
-  
-}
+    });
+  }
 }
